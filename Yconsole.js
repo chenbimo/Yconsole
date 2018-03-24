5eb95a9ec075f63217e2ca894895a2b17cd313d2
chensuiyilaya <bimostyle@qq.com>
2018-03-24 11:28:50 +0800

    Yconsole first commit

---
+++ Yconsole.js
@@ -0,0 +1,14 @@
+window.Yconsole = {
+    log:function(v){
+        console.log("%c"+v,"padding-left:16px;line-height:24px;background:url(http://x2zhp.com/public/img/1.png) no-repeat left center / 16px 16px");
+    },
+    info:function(v){
+        console.log("%c"+v,"padding-left:16px;line-height:24px;background:url(http://x2zhp.com/public/img/2.png) no-repeat left center / 16px 16px");
+    },
+    warn:function(v){
+        console.log("%c"+v,"padding-left:16px;line-height:24px;background:url(http://x2zhp.com/public/img/3.png) no-repeat left center / 16px 16px");
+    },
+    error:function(v){
+        console.log("%c"+v,"padding-left:16px;line-height:24px;background:url(http://x2zhp.com/public/img/4.png) no-repeat left center / 16px 16px");
+    }
+}
